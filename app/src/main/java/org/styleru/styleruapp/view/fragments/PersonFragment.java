63cdae3d5c6b4b37c9e8762bfd3be82f22b6b520
Dimagudilin <dima.gudilin@mail.ru>
2017-03-18 11:21:21 +0300

    Профиль человека (исправить потом цвета)

--- app/src/main/java/org/styleru/styleruapp/view/fragments/PersonFragment.java
+++ app/src/main/java/org/styleru/styleruapp/view/fragments/PersonFragment.java
@@ -1,13 +1,17 @@
 package org.styleru.styleruapp.view.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import org.styleru.styleruapp.MainActivity;
 import org.styleru.styleruapp.R;
 
 
@@ -18,13 +22,18 @@ public class PersonFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    Button btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("FRAG","person");
+        View view = inflater.inflate(R.layout.fragment_profile_mine, container, false);
+        MainActivity activity = (MainActivity) getActivity();
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        toolbar.setTitle("Мой профиль");
+        btn = (Button) view.findViewById(R.id.but_change);
+        btn.setTextColor(Color.rgb(58,81,85));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_mine, container, false);
+        return view;
     }
     @Override
     public void onStop() {
